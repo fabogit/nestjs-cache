c1389319ade827f205165beb527e69b7bbc4ed60
fabogit <fabo.dev.email@gmail.com>
2025-02-22 18:05:06 +0100

    #3
    added docker compose for redis container and refactor to use redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    CacheModule.register({
-      // ttl: 5 * 1000,
-      isGlobal: true,
+    CacheModule.registerAsync({
+      useFactory: () => {
+        return {
+          stores: [
+            // Redis Keyv instance
+            createKeyv('redis://localhost:6379', {}),
+          ],
+        };
+      },
     }),
   ],
   controllers: [AppController],
